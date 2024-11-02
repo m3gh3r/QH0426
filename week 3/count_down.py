9356addf3625b4c31e8fdb803c672ceece9db6f5
m3gh3r <valeriu.coman84@gmail.com>
2024-11-02 12:29:16 +0000

    number_of_steps.py

---
+++ week 3/count_down.py
@@ -0,0 +1,9 @@
+print("How far are we from the target?")
+no_of_steps = int(input())
+
+for count in range (no_of_steps , 0  ,-1):
+    print(f"{count} Steps are remaining")
+
+print("Target achieved!")
+
+
