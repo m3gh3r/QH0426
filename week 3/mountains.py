80cad34f249925cee6a6ba109cbde01c2237f4f8
m3gh3r <valeriu.coman84@gmail.com>
2024-11-02 12:17:50 +0000

    mountains.py

---
+++ week 3/mountains.py
@@ -0,0 +1,16 @@
+print("How many mountains should I display?")
+
+display = int(input())
+
+for mountain in range(display):
+     print("""
+            __
+           /    \_
+          /^      \\|
+         /  ^      \\_
+       _/ ^ ^       \\|
+       /  ^     ^     \\|
+       
+      """)
+
+
